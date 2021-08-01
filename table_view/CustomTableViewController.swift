5675c38e0cde90fb6f59d779c6d231dc60ecdc98
taehunlim <dsa1520@naver.com>
2021-08-01 15:20:30 +0900

    Separate Section

--- table_view/CustomTableViewController.swift
+++ table_view/CustomTableViewController.swift
@@ -48,20 +48,65 @@ extension CustomTableViewController: UITableViewDataSource, UITableViewDelegate 
     
     // table view, table view cell 의 row 값을 auto에서 원하는 값으지로 지정
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return restaurantNames.count
+        if section == 0 {
+            return restaurantNames.count
+        } else {
+            return restaurantNames.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let customCell = customTableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomTableViewCell
-        
-        customCell.nameLabel.text = restaurantNames[indexPath.row]
-        customCell.typeLabel.text = restaurantTypes[indexPath.row]
-        customCell.locationLabel.text = restaurantLocations[indexPath.row]
-        
-        customCell.cellImg.image = UIImage(named: restaurantImages[indexPath.row])
-        
-        return customCell
+        if indexPath.section == 0 {
+            let customCell = customTableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomTableViewCell
+            
+            customCell.nameLabel.text = restaurantNames[indexPath.row]
+            customCell.typeLabel.text = restaurantTypes[indexPath.row]
+            customCell.locationLabel.text = restaurantLocations[indexPath.row]
+            
+            customCell.cellImg.image = UIImage(named: restaurantImages[indexPath.row])
+            
+            return customCell
+        } else {
+            let rightCell = customTableView.dequeueReusableCell(withIdentifier: "rightCell", for: indexPath)
+            
+            rightCell.textLabel!.text = restaurantNames[indexPath.row]
+            rightCell.detailTextLabel!.text = restaurantLocations[indexPath.row]
+            
+            return rightCell
+            
+        }
+    
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return 30
+        } else {
+            return 10
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "custom section"
+        } else {
+            return "basic section"
+        }
+    }
+    
+    
+    // storyboard에서도 설정이 가능하며, 둘다 작성시 코드가 우선순위
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return 150
+        } else {
+            return 50
+        }
     }
 }
